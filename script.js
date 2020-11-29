eeb390237cb5a6c255b142d45c60710e71997e20
Dylan Jacobson <dylanrobertjacobson@gmail.com>
2020-11-29 23:19:19 +0000

    Adds button control

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ function divide(firstVar, secondVar) {
 function operate(firstVar, secondVar, operation) {
   return window[operation](firstVar, secondVar);
 }
+
+let buttonArray = document.querySelectorAll('.button');
+let equalsButton = document.getElementById('equals');
+let operator = '';
+let firstVarMode = true;
+let firstVar = '';
+let secondVar = '';
+
+buttonArray.forEach(button => button.addEventListener('click', function(){
+  if (button.classList.contains('operator')) {
+    if (operator === '') {
+      operator = button.id;
+      firstVarMode = false;
+    }
+  } else if (button.classList.contains('number')) {
+    if (firstVarMode) {
+      firstVar += button.textContent;
+    } else {
+      secondVar += button.textContent;
+    }
+  }
+}))
+
+equalsButton.addEventListener('click', function() {
+  if (firstVar.length > 0 && secondVar.length > 0) {
+    
+    let solution = operate(parseInt(firstVar), parseInt(secondVar), operator);
+    console.log(solution);
+    
+    clearVariables();
+  }
+})
+
+function clearVariables() {
+  operator = '';
+  firstVarMode = true;
+  firstVar = '';
+  secondVar = '';
+}
