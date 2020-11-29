b598864c993b5d710093b9d6cca52affa706eee8
Dylan Jacobson <dylanrobertjacobson@gmail.com>
2020-11-29 23:25:12 +0000

    Adds display output

--- script.js
+++ script.js
@@ -15,11 +15,22 @@ function divide(firstVar, secondVar) {
 }
 
 function operate(firstVar, secondVar, operation) {
-  return window[operation](firstVar, secondVar);
+  // return window[operation](firstVar, secondVar);
+  switch (operation) {
+    case '+':
+      return add(firstVar, secondVar);
+    case '-':
+      return subtract(firstVar, secondVar);
+    case 'x':
+      return multiply(firstVar, secondVar);
+    case '/':
+      return divide(firstVar, secondVar);
+  }
 }
 
 let buttonArray = document.querySelectorAll('.button');
 let equalsButton = document.getElementById('equals');
+let displayDiv = document.getElementById('display');
 let operator = '';
 let firstVarMode = true;
 let firstVar = '';
@@ -28,7 +39,7 @@ let secondVar = '';
 buttonArray.forEach(button => button.addEventListener('click', function(){
   if (button.classList.contains('operator')) {
     if (operator === '') {
-      operator = button.id;
+      operator = button.textContent;
       firstVarMode = false;
     }
   } else if (button.classList.contains('number')) {
@@ -38,12 +49,14 @@ buttonArray.forEach(button => button.addEventListener('click', function(){
       secondVar += button.textContent;
     }
   }
+  displayDiv.textContent = firstVar + operator + secondVar;
 }))
 
 equalsButton.addEventListener('click', function() {
   if (firstVar.length > 0 && secondVar.length > 0) {
     
     let solution = operate(parseInt(firstVar), parseInt(secondVar), operator);
+    displayDiv.textContent = solution;
     console.log(solution);
     
     clearVariables();
